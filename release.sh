a0d7250ef89ca3ce3328d9032b7a38727643ef3f
OneCDOnly <one.cd.only@gmail.com>
2024-05-12 15:12:56 +1000

    include release script

---
+++ release.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+package_name=SortMyQPKGs
+main_path=~/scripts/nas/$package_name
+
+[[ -d $main_path ]] || exit
+
+release_build=$(grep '^QPKG_VER=' $main_path/qpkg.cfg | cut -d '"' -f2)
+asset_pathfile="$main_path/build/${package_name}_${release_build}.qpkg"
+
+[[ -e $asset_pathfile ]] || exit
+
+git tag "v${release_build}"
+git push --tags
+
+gh release create "v${release_build}" --generate-notes "$asset_pathfile"
